5f2e3ab9e0f3c7cb26f009389a2470e590eef8e8
AdamRichards98 <adamc.richards98@gmail.com>
2025-05-21 21:41:32 -0500

    Added Tetris Grid

--- src/main.py
+++ src/main.py
@@ -1,20 +1,31 @@
+import itertools
 import pygame
 import sys
+from config import *
+
+def draw_grid(screen):
+    for x, y in itertools.product(range(GRID_WIDTH), range(GRID_HEIGHT)):
+        rect = pygame.Rect(x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+        pygame.draw.rect(screen, GRID_COLOR, rect, 1)
+
 
 def main():
     pygame.init() # Initialize Pygame
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Tetris")
 
     clock = pygame.time.Clock() # Create a clock object to control the frame rate
 
     while True:
+        
+        screen.fill(BACKGROUND_COLOR) # Fill the screen with the background color
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-        screen.fill((0, 0, 0))
+        draw_grid(screen) # Draw the grid
         pygame.display.flip()
         clock.tick(60)
 
